c669f712367f53685ebb8669ae5b74a689d027b3
vvkkr1 <vivek.kumarr1107@gmail.com>
2023-11-30 22:43:48 +0530

    user-directory-change

--- src/components/user-directory/index.js
+++ src/components/user-directory/index.js
@@ -1,14 +1,21 @@
 import React, { useState } from 'react'
 import "./userDirectory.css";
 
+
+
 const UserDirectory = () => {
   
   return (
     <div className='user-directory'>
-        <div className='user-directory-wrap'>
-            <div className='title'>Name</div>
-            <div className='posts'>Posts</div>
-        </div>
+          <div className='title-directory'>Directory</div>
+          <div className='user-directory-wrap'>
+                <div className='title'>Name: Person</div>
+                <div className='posts'>Posts: 12</div>
+          </div>
+          <div className='user-directory-wrap'>
+                <div className='title'>Name: Person</div>
+                <div className='posts'>Posts: 12</div>
+          </div>
     </div>
   )
 
